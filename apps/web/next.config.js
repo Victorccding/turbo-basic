bc8b9d0bb02cc1e688643c9b15da8d30842ab61c
Victorccding <82922344+Victorccding@users.noreply.github.com>
2023-04-13 21:33:25 -0700

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,8 @@
+const path = require('path')
 module.exports = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../")
+  }
 };
